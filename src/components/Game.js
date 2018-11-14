38aa72fd08369be04506de333aff635ac7020fa5
Desko Sekulović <desko.sekulovic@gmail.com>
2018-11-14 17:47:32 +0100

    Added canvas in game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.myRef = React.createRef();
+  }
   render() {
     return (
       <div>
-        <h1>Game</h1>
+        <div style={{ padding: '10px', fontSize: '18px' }}>
+          <h3>
+            Result: <span />
+          </h3>
+        </div>
+        <canvas
+          ref={this.myRef}
+          width="800px"
+          height="800px"
+          style={{ border: '1px solid #000000', backgroundColor: 'black' }}
+        />
       </div>
     );
   }
