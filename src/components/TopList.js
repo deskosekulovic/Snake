dacaf248a5e0523dbd6e7929b850d46acda2750a
Desko Sekulović <desko.sekulovic@gmail.com>
2018-11-15 18:10:38 +0100

    basic layout for toplist

--- src/components/TopList.js
+++ src/components/TopList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import StyledTopList, { Table } from '../styles/StyledTopList';
 
 class TopList extends Component {
   render() {
     return (
-      <div>
-        <h1>Top list!</h1>
-      </div>
+      <StyledTopList>
+        <h2>List</h2>
+        <br />
+        <h2>Top 10</h2>
+        <Table>
+          <tbody>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+            <tr>
+              <td>data</td>
+              <td>data</td>
+              <td>data</td>
+            </tr>
+          </tbody>
+        </Table>
+      </StyledTopList>
     );
   }
 }
