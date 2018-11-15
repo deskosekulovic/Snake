6c3875712856d6c49ae8370fc2c109c21f8cf42e
Desko Sekulović <desko.sekulovic@gmail.com>
2018-11-15 13:46:49 +0100

    layout added

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,58 +1,73 @@
 import React, { Component } from 'react';
+import StyledSettings, {
+  Form1,
+  Form2,
+  Block1,
+  Block2,
+  Block3
+} from '../styles/StyledSettings';
 
 class Settings extends Component {
   render() {
     return (
-      <div>
+      <StyledSettings>
         <h1>Welcome to settings!</h1>
         <hr />
-        <form>
-          <label>
-            <h3>Pick size</h3>
-
-            <select value="" name="size">
-              <option value="40">Big</option>
-              <option value="30">Medium</option>
-              <option value="20">Small</option>
-            </select>
-          </label>
-
-          <h3>Walls</h3>
-          <input type="checkbox" name="walls" checked="false" />
-
-          <h3>Speed: </h3>
-          <input type="range" min="10" max="100" step="10" name="speed" />
-        </form>
+        <Form1>
+          <Block1>
+            <label>
+              <h3>Pick size</h3>
+              <span>
+                <select value="" name="size">
+                  <option value="40">Big</option>
+                  <option value="30">Medium</option>
+                  <option value="20">Small</option>
+                </select>
+              </span>
+            </label>
+            <label>
+              <h3>Walls</h3>
+              <input type="checkbox" name="walls" checked="false" />
+            </label>
+          </Block1>
+          <Block3>
+            <h3>Speed: </h3>
+            <input type="range" min="10" max="100" step="10" name="speed" />
+          </Block3>
+        </Form1>
         <hr />
-        <form>
+        <Form2>
           <h2>Adjust controls</h2>
-
-          <label className="container">
-            <h3>Left</h3>
-            <input type="text" name="left" value="" />
-          </label>
-          <label className="container">
-            <h3>Right</h3>
-            <input type="text" name="right" value="" />
-          </label>
-          <label className="container">
-            <h3>Up</h3>
-            <input type="text" name="up" value="" />
-          </label>
-          <label className="container">
-            <h3>Down</h3>
-            <input type="text" name="down" value="" />
-          </label>
-          <label className="container">
-            <h3>Start</h3>
-            <input type="text" name="start" value="" />
-          </label>
-          <label className="container">
-            <h3>Replay</h3>
-            <input type="text" name="replay" value="" />
-          </label>
-        </form>
-      </div>
+          <Block1>
+            <label className="container">
+              <h3>Left</h3>
+              <input type="text" name="left" value="" />
+            </label>
+            <label className="container">
+              <h3>Right</h3>
+              <input type="text" name="right" value="" />
+            </label>
+            <label className="container">
+              <h3>Up</h3>
+              <input type="text" name="up" value="" />
+            </label>
+            <label className="container">
+              <h3>Down</h3>
+              <input type="text" name="down" value="" />
+            </label>
+          </Block1>
+          <Block2>
+            <label className="container">
+              <h3>Start</h3>
+              <input type="text" name="start" value="" />
+            </label>
+            <label className="container">
+              <h3>Replay</h3>
+              <input type="text" name="replay" value="" />
+            </label>
+          </Block2>
+        </Form2>
+      </StyledSettings>
     );
   }
 }
